343d5214424d7ad39c819a787546046647a769bb
Sammy Macharia <Sammymacharia1997@gmail.com>
2019-05-01 22:50:39 +0300

    chat for admin and student

---
+++ app/Message.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Message extends Model
+{
+    //
+    // app/Message.php
+
+/**
+ * Fields that are mass assignable
+ *
+ * @var array
+ */
+protected $fillable = ['message'];
+
+
+/**
+ * A message belong to a user
+ *
+ * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+ */
+public function user()
+{
+  return $this->belongsTo(User::class);
+}
+}
---
+++ resources/js/components/chatMessages.vue
@@ -0,0 +1,23 @@
+<template>
+    <ul class="chat">
+        [vue/require-v-for-key]
+        <li class="left clearfix" v-for="message in messages">
+            <div class="chat-body clearfix">
+                <div class="header">
+                    <strong class="primary-font">
+                        {{ message.user.name }}
+                    </strong>
+                </div>
+                <p>
+                    {{ message.message }}
+                </p>
+            </div>
+        </li>
+    </ul>
+</template>
+
+<script>
+  export default {
+    props: ['messages']
+  };
+</script>
