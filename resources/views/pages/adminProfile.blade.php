c6b70acfc4e0cc92ba7f4c59a367f38153bd908a
mulehi <freeman.mulei@gmail.com>
2019-05-14 09:39:34 -0700

    created admin profile

--- resources/views/pages/adminProfile.blade.php
+++ resources/views/pages/adminProfile.blade.php
@@ -1,1 +1,22 @@
-this is the admin profile
+@extends('layouts.apptry');
+
+@section('content')
+    <div class="container col-8 offset-2 d-flex">
+        <div>
+            <h2>Profile Picture</h2>
+        </div>
+        <div>
+            <div class="form-group">
+                <label for="id">Admin ID</label> 
+            </div>
+            <div class="form-group">
+                <label for="name">Admin Name</label> 
+            </div>
+            <div class="form-group">
+                <label for="email">Admin Email</label> 
+            </div>
+        </div>
+
+    </div>
+    
+@endsection
